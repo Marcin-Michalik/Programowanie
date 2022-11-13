70b04296a12f94fd786adc7693316db1d78d1b08
Marcin <marcin33123@gmail.com>
2022-11-13 12:47:08 +0100

    dodanie menu (2.3)

--- mzf.cpp
+++ mzf.cpp
@@ -46,76 +46,77 @@ int main()
 /////////     MENU   ///////////
 {	int x;
 	float a, b, c;
-	cout <<"miejsce zerowe ax+b - wcisnij 1"<<endl;
-	cout <<"miejsce zerowe Ax+By+C=0 - wcisnij 2"<<endl;
-	cout <<"miejsce zerowe postaci ax2 + bx + c - wcisnij 3"<<endl;
-	cin>>x;
+	int z=0;
 	
-	
-	
-	
-	
-	///////////		ax+b	//////////
-	if(x==1){
-	
-		cout << "podaj a:";
-		cin >> a;
-		cout << "podaj b:";
-		cin >> b;
+	do{
+			do{
+			cout << "-----   MENU WYBORU FUNKJCI   -----"<<endl<<endl;
+			cout <<"( miejsce zerowe ax+b ) -  wcisnij 1"<<endl;
+			cout <<"( miejsce zerowe Ax+By+C=0 ) -  wcisnij 2"<<endl;
+			cout <<"( miejsce zerowe postaci ax2 + bx + c ) -  wcisnij 3"<<endl;
+			cin>>x;
+		}while( x>3 || x<1 );
+		
+		
+		///////////		ax+b	//////////
+		if(x==1){
+		
+			cout << "podaj a:";
+			cin >> a;
+			cout << "podaj b:";
+			cin >> b;
+				
+			if (a==0.0){
+				if(b==0.0)
+					cout<<"a = 0, b = 0, wiec miejsce zerowe jest rowne 0.\n";
+				else
+					cout<<"Brak miejsc zerowych funkcji.\n";
+			}
+			else{
+				cout<<"funkcja "<<a<<"x +"<<b<<" ma miejsce zerowe:";
+				cout<<f_liniowa_0(a, b);	
+			}
+		}
+		
+		
+		///////////		Ax + By + C = 0 	//////////
+		else if(x==2){
+			cout << "podaj a:";
+			cin >> a;
+			cout << "podaj b:";
+			cin >> b;
+			cout << "podaj c:";
+			cin >> c;
 			
-		if (a==0.0){
-			if(b==0.0)
-				cout<<"a = 0, b = 0, wiec miejsce zerowe jest rowne 0.\n";
-			else
-				cout<<"Brak miejsc zerowych funkcji.\n";
-		}
-		else{
-			cout<<"funkcja "<<a<<"x +"<<b<<" ma miejsce zerowe:";
-			cout<<f_liniowa_0(a, b);	
-		}
-	}
-	
-	
-	///////////		Ax + By + C = 0 	//////////
-	else if(x==2){
-		cout << "podaj a:";
-		cin >> a;
-		cout << "podaj b:";
-		cin >> b;
-		cout << "podaj c:";
-		cin >> c;
-		
-		cout << "Funkcja "<<a<<"x + "<<b<<"y +"<<c<<"= 0 ma miejsce zerowe: ";
-		cout<<f_liniowa_3D(a,b,c);
-	}
-	
-	
-	///////////		ax2 + bx + c	//////////
-	else if(x==3){
-		cout << "podaj a:";
-		cin >> a;
-		cout << "podaj b:";
-		cin >> b;
-		cout << "podaj c:";
-		cin >> c;
-		if(a==0){
-			cout << "jest to funkcja kwadratowa z mijescem zerowym x="<<f_liniowa_0(b,c);
-			 
-		}
-		else{
-			f_kwadratowa(a,b,c);
+			cout << "Funkcja "<<a<<"x + "<<b<<"y +"<<c<<"= 0 ma miejsce zerowe: ";
+			cout<<f_liniowa_3D(a,b,c);
 		}
 		
-	}
+		
+		///////////		ax2 + bx + c	//////////
+		else if(x==3){
+			cout << "podaj a:";
+			cin >> a;
+			cout << "podaj b:";
+			cin >> b;
+			cout << "podaj c:";
+			cin >> c;
+			if(a==0){
+				cout << "jest to funkcja kwadratowa z mijescem zerowym x="<<f_liniowa_0(b,c);
+				 
+			}
+			else{
+				f_kwadratowa(a,b,c);
+			}	
+		}
+		
+	cout << endl << endl<< endl << endl;		
+	}while(z==0);
 	
 	
 	
 	
-	
-	else{
-		cout << "brak";
-	}
-	
+
 
 
 	return 0;
