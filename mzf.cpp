e765a4a3ea84c13e5831f87b89669ac15189ec4f
Marcin <marcin33123@gmail.com>
2022-11-13 12:32:54 +0100

    możliwość podania a=0  ver1.2

--- mzf.cpp
+++ mzf.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
-
-
 float f_liniowa_0(float a,float b) {
 	return -(b / a);
 }
-
-
 int main()
 {
 		float a, b;
@@ -17,15 +12,17 @@ int main()
 		cout << "podaj b:";
 		cin >> b;
 
-		if (a==0){
-			cout<<"a nie moze byæ równe 0";
+
+		if (a==0.0){
+			if(b==0.0)
+				cout<<"a = 0, b = 0, wiec miejsce zerowe jest rowne 0.\n";
+			else
+				cout<<"Brak miejsc zerowych funkcji.\n";
 		}
 		else{
 			cout<<"funkcja "<<a<<"x +"<<b<<" ma miejsce zerowe:";
 			cout<<f_liniowa_0(a, b);	
 		}
-
-
-
+		
 	return 0;
 }
