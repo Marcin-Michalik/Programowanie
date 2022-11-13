176ce59f6cdb4e4e4d26391fa7ce2b96ecb93608
Marcin <marcin33123@gmail.com>
2022-11-13 12:39:58 +0100

    obliczanie miejsc zerowych funkcji w postaci Ax + By + C = 0 (1.4

--- mzf.cpp
+++ mzf.cpp
@@ -4,25 +4,42 @@ using namespace std;
 float f_liniowa_0(float a,float b) {
 	return -(b / a);
 }
+
+
+float f_liniowa_3D(float a,float b, float c) {
+	return -(c/a);
+}
 int main()
-{
-		float a, b;
+
+{	int x;
+	float a, b, c;
+	cout <<"miejsce zerowe ax+b - wcisnij 1"<<endl;
+	cout <<"miejsce zerowe Ax+By+C=0 - wcisnij 2"<<endl;
+	cin>>x;
+
+	if(x==1){
+	
+		cout << "podaj a:";
+		cin >> a;
+		cout << "podaj b:";
+		cout<<"funkcja "<<a<<"x +"<<b<<" ma miejsce zerowe:";
+			cout<<f_liniowa_0(a, b);	
+		}
+	
+	else{
 		cout << "podaj a:";
 		cin >> a;
 		cout << "podaj b:";
 		cin >> b;
+		cout << "podaj c:";
+		cin >> c;
+
+		cout << "Funkcja "<<a<<"x + "<<b<<"y +"<<c<<"= 0 ma miejsce zerowe: ";
+		cout<<f_liniowa_3D(a,b,c);
+	}
 
 
-		if (a==0.0){
-			if(b==0.0)
-				cout<<"a = 0, b = 0, wiec miejsce zerowe jest rowne 0.\n";
-			else
-				cout<<"Brak miejsc zerowych funkcji.\n";
-		}
-		else{
-			cout<<"funkcja "<<a<<"x +"<<b<<" ma miejsce zerowe:";
-			cout<<f_liniowa_0(a, b);	
-		}
-		
+
+
 	return 0;
 }
